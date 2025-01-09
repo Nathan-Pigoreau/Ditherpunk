b49727a05f9aa74222bbee8ea97a5e5065eb24ab
nathan-pigoreau <nathan.pigoreau@etu.univ-orleans.fr>
2025-01-09 12:05:23 +0100

    Implémentation de la question 2 et 3

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Ditherpunk
+# Ditherpunk: Retour au Monochrome
+
+**Binôme : Nathan Pigoreau & Malleron Daniel**
+
+### Description du projet
+
+Dans ce projet, nous allons manipuler des images en utilisant la bibliothèque **Rust `image`** pour appliquer plusieurs traitements visant à transformer une image en une version réduite à une palette de couleurs. Ces traitements incluent la conversion d'images en monochrome, l'application d'une palette de couleurs, ainsi que l'utilisation de techniques de tramage telles que le **dithering**.
+
+L'énoncé de ce TP est largement inspiré de la page [Ditherpunk](https://surma.dev/things/ditherpunk/) et nous avons pour objectif de créer une application en ligne de commande permettant de :
+- Sélectionner une image en entrée.
+- Sélectionner un nom pour le fichier de sortie (par défaut, `out.png`).
+- Choisir et appliquer différents traitements à l'image.
+
+### Objectifs
+
+1. **Manipulation des images avec la bibliothèque `image`** : ouvrir, transformer, et sauvegarder des images.
+2. **Passage en monochrome** : convertir une image en noir et blanc par seuil ou par palettisation.
+3. **Tramage (Dithering)** : utiliser des techniques de tramage pour ajouter du bruit et des nuances dans les images.
+
+## Installation et lancement
+1. Clonez le dépôt : 
+```bash
+git clone https://github.com/Nathan-Pigoreau/Ditherpunk.git
+```
+2. Assurez-vous d'avoir **Rust** installé sur votre machine. Si ce n'est pas le cas, vous pouvez l'installer depuis [https://www.rust-lang.org/](https://www.rust-lang.org/).
+3. Aller dans le dossier du projet rust :
+```bash
+cd  Ditherpunk/ditherpunk
+```
+4. Exécutez le programme :
+```bash
+cargo run
+```
+
--- ditherpunk/src/main.rs
+++ ditherpunk/src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use argh::FromArgs;
+use image::{GenericImageView, ImageFormat};
+
+#[derive(Debug, Clone, PartialEq, FromArgs)]
+/// Convertit une image en monochrome ou vers une palette réduite de couleurs.
+struct DitherArgs {
+
+    /// le fichier d’entrée
+    #[argh(positional)]
+    input: String,
+
+    /// le fichier de sortie (optionnel)
+    #[argh(positional)]
+    output: Option<String>,
+
+    /// le mode d’opération
+    #[argh(subcommand)]
+    mode: Mode
 }
+
+#[derive(Debug, Clone, PartialEq, FromArgs)]
+#[argh(subcommand)]
+enum Mode {
+    Seuil(OptsSeuil),
+    Palette(OptsPalette),
+}
+
+#[derive(Debug, Clone, PartialEq, FromArgs)]
+#[argh(subcommand, name="seuil")]
+/// Rendu de l’image par seuillage monochrome.
+struct OptsSeuil {}
+
+
+#[derive(Debug, Clone, PartialEq, FromArgs)]
+#[argh(subcommand, name="palette")]
+/// Rendu de l’image avec une palette contenant un nombre limité de couleurs
+struct OptsPalette {
+
+    /// le nombre de couleurs à utiliser, dans la liste [NOIR, BLANC, ROUGE, VERT, BLEU, JAUNE, CYAN, MAGENTA]
+    #[argh(option)]
+    n_couleurs: usize
+}
+
+fn main() {
+    let args: DitherArgs = argh::from_env();
+    let path_in = args.input;
+    let mut path_out = args.output.unwrap_or_else(|| "output.png".to_string());
+    if !path_out.contains("/img/"){
+        path_out = format!("img/{}", path_out);
+    }
+    let mode = args.mode;
+    let img = image::open(path_in).expect("Impossible d'ouvrir l'image");
+    let rgb_image = img.to_rgb8();
+    rgb_image.save_with_format(path_out, ImageFormat::Png).expect("Erreur de sauvegarde");
+    println!("Image convertie et sauvegardée avec succès");
+    // Ok(())
+}
+
